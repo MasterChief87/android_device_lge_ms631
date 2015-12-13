31c4708c35c2645de1cc0bc3155f7a458fa80f3f
MasterChief87 <mc87xda@gmail.com>
2015-12-13 12:47:10 -0500

    Update broken.mk

--- broken.mk
+++ broken.mk
@@ -8,6 +8,6 @@ $(call inherit-product, vendor/broken/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_ms631
+PRODUCT_NAME := broken_ms631
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g4stylusn" PRODUCT_NAME="g4stylusn_mpcs_us" BUILD_FINGERPRINT="MetroPCS/g4stylusn_mpcs_us/g4stylusn:5.1.1/LMY47V/152171011984e:user/release-keys" PRIVATE_BUILD_DESC="g4stylusn_mpcs_us-user 5.1.1 LMY47V 152171011984e release-keys"
